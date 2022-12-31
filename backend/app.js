eb70d671360bbeccbd7f4739435b2ec3142d9c0d
Nathnael_D <109980176+NathanZlion@users.noreply.github.com>
2022-12-31 08:29:41 +0300

    login and signup page successfully made

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+
+import express from "express";
+import mongoose from 'mongoose';
+import router from "./routes/user-routes";
+
+const app = express();
+
+app.use(express.json());
+// parse all data in json format
+
+app.use("/api/user", router);
+// http:localhost/api/user
+
+
+mongoose.set('strictQuery', false);
+
+// mongodb://localhost:27017/zblog
+mongoose
+    .connect("mongodb://127.0.0.1:27017/zblog")
+    .catch((err) => {
+        return console.log(err);
+    })
+    .then(console.log("Data base connected to 127.0.0.1:5000!"))
+    .catch((err) => {
+        return console.log(err);
+    });
+
+
+// app.use("/api", (req, res, next) => {
+//     res.send("Hello world.")
+// });
+
+
+app.listen(5000);
---
+++ backend/controllers/user-controller.js
@@ -0,0 +1,85 @@
+
+
+import User from '../models/user';
+import bcrypt from 'bcryptjs';
+
+export const getAllUsers = async (req, res, next) => {
+    let users;
+
+    try {
+        users = await User.find();
+    } catch (error) {
+        return console.log(error);
+    }
+
+    if (!users) {
+        return res.status(404).json({ message: "no users found!" });
+    }
+    return res.status(200).json({ users });
+}
+
+export const signup = async (req, res, next) => {
+    try {
+
+        const { name, email, password } = req.body;
+
+        let existingUser;
+
+        try {
+            existingUser = await User.findOne({ email });
+        } catch (error) {
+            return console.log(error);
+        }
+
+        if (existingUser) {
+            return res.status(400).json({ message: "User account already exists, Login instead." })
+        }
+
+
+        const hashedpassword = bcrypt.hashSync(password);
+        // if user is really new
+        const user = new User({
+            name,
+            email,
+            password: hashedpassword,
+        });
+
+        try {
+            user.save();
+        } catch (error) {
+            return console.log(error);
+        }
+
+        return res.status(201).json({ user })
+    } catch (error) {
+        return console.log(error);
+    }
+}
+
+
+export const login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    let existingUser;
+    existingUser = await User.findOne({ email });
+
+
+    if (!existingUser) {
+        return res
+            .status(400)
+            .json({ message: "User Doesn't exist by that email." })
+    }
+
+    // if that email exists compare the password, returns a boolean
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+    if (!isPasswordCorrect) {
+        // 400: unauthorized
+        return res
+            .status(400)
+            .json({ message: "Incorrect Password" });
+    }
+
+    return res
+        .status(200)
+        .json({ message: "login successfull" });
+}
